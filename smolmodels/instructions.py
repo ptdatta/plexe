048101b1cfefb6d2f8c8ff4af2ebaf5c8dffdae0
marcellodebernardi <marcello.logins@outlook.com>
2025-01-10 19:05:07 +0000

    Add a todo to instructions.py

--- smolmodels/instructions.py
+++ smolmodels/instructions.py
@@ -1,6 +1,6 @@
 # todo: as discussed we need a better name for the 'instruction' class, so we can rename the module
 # and the class once we find it
-
+# todo: we also need to figure out what an instruction really is 😂?
 
 class Instruction:
     pass
