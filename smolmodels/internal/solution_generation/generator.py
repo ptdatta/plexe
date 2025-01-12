c1ebb2502dcef81222b83ee94477cbd061af0e00
marcellodebernardi <marcello.logins@outlook.com>
2025-01-12 17:09:07 +0000

    Delete generator.py as it is not currently used

--- smolmodels/internal/solution_generation/generator.py
+++
@@ -1,8 +0,0 @@
-# todo: implement - this module is intended to be the entry point into the solution generation logic
-
-
-# todo: this function signature is garbage, fix it
-def generate(
-    problem_statement, input_schema, output_schema, constraints, data, generation_directives, callbacks, isolation
-) -> None:
-    return None
